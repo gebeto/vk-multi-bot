4c2ca5b1f828844ce75edce84c58e723c0b80e4b
gebeto <bboyheadman@gmail.com>
2017-06-12 18:01:58 +0300

    FieldsTypes renamed to FieldType

--- tables/TableFieldsTypes.php
+++ tables/TableFieldsTypes.php
@@ -3,9 +3,10 @@
 /**
 * TableFieldsTypes
 */
-class FieldsTypes
+class FieldType
 {
 	static public $ID_FIELD = '`id` INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(`id`)';
+	static public $NUMBER_FIELD = '`number` INT NOT NULL';
 }
 
 ?>
